0a09c9a3c169e35418651fe9f28e6e6253d10336
Aidan Thorne <thaaidstar@gmail.com>
2021-06-08 09:30:10 +1000

    Added Homing Projectile abstract classes

---
+++ Projectiles/Homing/HomingProjectile.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerraLeague.Projectiles.Homing
+{
+    public abstract class HomingProjectile : ModProjectile
+    {
+        public int targetingRange = 450;
+        public float MaxVelocity = -1;
+        public float TurningFactor = 0;
+        public bool CanOnlyHitTarget = false;
+        public bool NPC_CanTargetDummy = false;
+        public bool NPC_CanTargetCritters = false;
+        public bool CanRetarget = false;
+
+        public bool TargetPlayers = false;
+        public bool Player_OnlyTargetTeam = false;
+        public bool Player_CanTargetDead = false;
+
+        public int TargetWhoAmI { get { return (int)projectile.ai[0]; } set { projectile.ai[0] = value; } }
+        public bool CouldNotFindTarget { get { return -1 == (int)projectile.ai[0]; } }
+        public bool NeedsNewTarget { get { return -2 == (int)projectile.ai[0]; } }
+        public Entity TargetEntity { get { if (TargetPlayers) { return Main.player[TargetWhoAmI]; } else { return Main.npc[TargetWhoAmI]; } } }
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            if (projectile.owner == Main.LocalPlayer.whoAmI)
+            {
+                writer.Write(targetingRange);
+                writer.Write(MaxVelocity);
+                writer.Write(TurningFactor);
+                writer.Write(CanOnlyHitTarget);
+                writer.Write(NPC_CanTargetDummy);
+                writer.Write(NPC_CanTargetCritters);
+                writer.Write(CanRetarget);
+                writer.Write(TargetPlayers);
+                writer.Write(Player_OnlyTargetTeam);
+                writer.Write(Player_CanTargetDead);
+            }
+
+            base.SendExtraAI(writer);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            targetingRange = reader.ReadInt32(); ;
+            MaxVelocity = reader.ReadSingle(); ;
+            TurningFactor = reader.ReadSingle(); ;
+            CanOnlyHitTarget = reader.ReadBoolean();
+            NPC_CanTargetDummy = reader.ReadBoolean();
+            NPC_CanTargetCritters = reader.ReadBoolean();
+            CanRetarget = reader.ReadBoolean(); ;
+
+            TargetPlayers = reader.ReadBoolean();
+            Player_OnlyTargetTeam = reader.ReadBoolean();
+            Player_CanTargetDead = reader.ReadBoolean();
+            base.ReceiveExtraAI(reader);
+        }
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.Homing[projectile.type] = true;
+        }
+
+        public override void AI()
+        {
+            HomingAI();
+
+            base.AI();
+        }
+
+        public virtual void HomingAI()
+        {
+            if (MaxVelocity == -1)
+                MaxVelocity = projectile.velocity.Length();
+
+            if (NeedsNewTarget)
+            {
+                GetNewTarget();
+            }
+
+            if (CouldNotFindTarget)
+            {
+                CouldNotFindNewTarget();
+                return;
+            }
+
+
+            if (!TargetEntity.active)
+            {
+                TargetIsNotActive();
+            }
+            else
+            {
+                Vector2 NewVelocity = (TurningFactor * projectile.velocity) + ((1 - TurningFactor) * TerraLeague.CalcVelocityToPoint(projectile.Center, TargetEntity.Center, MaxVelocity));
+
+#pragma warning disable CS1718 // Comparison made to same variable
+                if (NewVelocity != NewVelocity)
+#pragma warning restore CS1718 // Comparison made to same variable
+                    projectile.velocity = Vector2.Zero;
+                else
+                    projectile.velocity = NewVelocity;
+            }
+        }
+
+        /// <summary>
+        /// Runs when the specified target in ai[0] is not active.
+        /// </summary>
+        public virtual void TargetIsNotActive()
+        {
+            if (CanRetarget)
+                GetNewTarget();
+            else
+                projectile.Kill();
+
+        }
+
+        public virtual void CouldNotFindNewTarget()
+        {
+            if (CanRetarget)
+                TargetWhoAmI = -2;
+            else
+                projectile.Kill();
+        }
+
+        public virtual void GetNewTarget()
+        {
+            projectile.netUpdate = true;
+            if (TargetPlayers)
+                TargetWhoAmI = TerraLeague.GetClosestPlayer(projectile.Center, targetingRange, -1, -1);
+            else
+                TargetWhoAmI = TerraLeague.GetClosestNPC(projectile.Center, targetingRange, -1, -1, NPC_CanTargetCritters, NPC_CanTargetDummy);
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            if (CanOnlyHitTarget)
+            {
+                return target.whoAmI == TargetWhoAmI;
+            }
+            else
+            {
+                return base.CanHitNPC(target);
+            }
+        }
+
+        public virtual bool IsHittingPlayer(Player player)
+        {
+            if (TargetPlayers)
+            {
+                return projectile.Hitbox.Intersects(player.Hitbox);
+            }
+
+            return false;
+        }
+
+        public virtual void OnHitFriendlyPlayer(Player player)
+        {
+            projectile.netUpdate = true;
+            projectile.penetrate--;
+        }
+    }
+}
---
+++ Projectiles/Homing/HomingRichochetProjectile.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerraLeague.Projectiles.Homing
+{
+    public abstract class RichochetProjectile : HomingProjectile
+    {
+        public int[] HaveHit;
+        public int hitCounter = 0;
+        public bool exlusiveTargeting = false;
+        public int PostHitTimeLeft = 300;
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            if (projectile.owner == Main.LocalPlayer.whoAmI)
+            {
+                for (int i = 0; i < HaveHit.Length; i++)
+                {
+                    writer.Write(HaveHit[i]);
+                }
+                writer.Write(hitCounter);
+            }
+
+            base.SendExtraAI(writer);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            for (int i = 0; i < HaveHit.Length; i++)
+            {
+                HaveHit[i] = reader.ReadInt32();
+            }
+            hitCounter = reader.ReadInt32();
+            base.ReceiveExtraAI(reader);
+        }
+
+        public override void AI()
+        {
+            base.AI();
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            projectile.netUpdate = true;
+            projectile.timeLeft = PostHitTimeLeft;
+
+            if (exlusiveTargeting)
+            {
+                HaveHit[hitCounter] = target.whoAmI;
+            }
+            hitCounter++;
+
+            GetNewTarget();
+
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+
+        public override void GetNewTarget()
+        {
+            projectile.netUpdate = true;
+
+            if (exlusiveTargeting)
+                TargetWhoAmI = TerraLeague.GetClosestNPC(projectile.Center, 480, HaveHit, true, true);
+            else
+                TargetWhoAmI = TerraLeague.GetClosestNPC(projectile.Center, 480);
+
+            if (!CouldNotFindTarget)
+            {
+                Main.npc[TargetWhoAmI].immune[projectile.owner] = 0;
+            }
+            else
+            {
+                projectile.Kill();
+            }
+        }
+
+        public void SetHomingDefaults(bool exclusiveTargeting, int targetingRange, int timeLeftAfterHit)
+        {
+            this.exlusiveTargeting = exclusiveTargeting;
+            this.targetingRange = targetingRange;
+            PostHitTimeLeft = timeLeftAfterHit;
+
+            if (exclusiveTargeting)
+            {
+                HaveHit = new int[projectile.penetrate];
+                for (int i = 0; i < HaveHit.Length; i++)
+                {
+                    HaveHit[i] = -1;
+                }
+            }
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            if (exlusiveTargeting)
+            {
+                if (!HaveHit.Contains(TargetWhoAmI) || (CanOnlyHitTarget && TargetWhoAmI == target.whoAmI))
+                    return base.CanHitNPC(target);
+                else
+                    return false;
+            }
+            else
+            {
+                return base.CanHitNPC(target);
+            }
+        }
+    }
+}
