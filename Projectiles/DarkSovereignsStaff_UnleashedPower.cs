0d205284cc5edd04ccd43fd3a340b91094bd9e93
Aidan Thorne <thaaidstar@gmail.com>
2021-06-13 10:26:11 +1000

    Updated Unleashed Power projectile to use new Homing Projectile code

--- Projectiles/DarkSovereignsStaff_UnleashedPower.cs
+++ Projectiles/DarkSovereignsStaff_UnleashedPower.cs
@@ -1,20 +1,21 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using TerraLeague.Projectiles.Homing;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace TerraLeague.Projectiles
 {
-	public class DarkSovereignsStaff_UnleashedPower : ModProjectile
+	public class DarkSovereignsStaff_UnleashedPower : HomingProjectile
 	{
         int totalProj = 1;
 
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Unleashed Power");
-            ProjectileID.Sets.Homing[projectile.type] = true;
+            base.SetStaticDefaults();
         }
 
         public override void SetDefaults()
@@ -30,6 +31,11 @@ namespace TerraLeague.Projectiles
             projectile.tileCollide = false;
             projectile.ignoreWater = true;
             projectile.GetGlobalProjectile<PROJECTILEGLOBAL>().abilitySpell = true;
+
+            CanOnlyHitTarget = true;
+            CanRetarget = false;
+            MaxVelocity = 12;
+            TurningFactor = 0.9f;
         }
 
         public override void AI()
@@ -70,40 +76,7 @@ namespace TerraLeague.Projectiles
                 dust.noGravity = true;
                 dust.noLight = true;
 
-                float velocity = (float)Math.Sqrt((double)(projectile.velocity.X * projectile.velocity.X + projectile.velocity.Y * projectile.velocity.Y));
-                float num133 = projectile.localAI[0];
-                if (num133 == 0f)
-                {
-                    projectile.localAI[0] = velocity;
-                    num133 = velocity;
-                }
-                float num134 = projectile.position.X;
-                float num135 = projectile.position.Y;
-                bool flag3 = false;
-
-                float num143 = target.position.X + (float)(target.width / 2);
-                float num144 = target.position.Y + (float)(target.height / 2);
-                if (Math.Abs(projectile.position.X + (float)(projectile.width / 2) - num143) + Math.Abs(projectile.position.Y + (float)(projectile.height / 2) - num144) < 1000f)
-                {
-                    flag3 = true;
-                    num134 = target.position.X + (float)(target.width / 2);
-                    num135 = target.position.Y + (float)(target.height / 2);
-                }
-
-                if (flag3)
-                {
-                    float num145 = num133;
-                    Vector2 vector10 = new Vector2(projectile.position.X + (float)projectile.width * 0.5f, projectile.position.Y + (float)projectile.height * 0.5f);
-                    float num146 = num134 - vector10.X;
-                    float num147 = num135 - vector10.Y;
-                    float num148 = (float)Math.Sqrt((double)(num146 * num146 + num147 * num147));
-                    num148 = num145 / num148;
-                    num146 *= num148;
-                    num147 *= num148;
-                    int num149 = 8;
-                    projectile.velocity.X = (projectile.velocity.X * (float)(num149 - 1) + num146) / (float)num149;
-                    projectile.velocity.Y = (projectile.velocity.Y * (float)(num149 - 1) + num147) / (float)num149;
-                }
+                HomingAI();
             }
             else
             {
@@ -146,15 +119,6 @@ namespace TerraLeague.Projectiles
                 0f
             );
             base.PostDraw(spriteBatch, lightColor);
-        }
-
-        public override bool? CanHitNPC(NPC target)
-        {
-            if (target.whoAmI == (int)projectile.ai[0])
-                return base.CanHitNPC(target);
-            else
-                return false;
-
         }
     }
 }
