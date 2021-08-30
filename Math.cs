c9b0777bd1957b6de5c6d0611c6189e12e9d02f9
Aidan Thorne <thaaidstar@gmail.com>
2021-08-30 16:37:14 +1000

    Added static class Math to hold all calculation based methods

---
+++ Math.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraLeague
+{
+    public static class Math
+    {
+        public static int GetBetween(this int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            else
+                return value;
+        }
+
+        public static float GetBetween(this float value, float min, float max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            else
+                return value;
+        }
+
+        public static double GetBetween(this double value, double min, double max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            else
+                return value;
+        }
+    }
+}
